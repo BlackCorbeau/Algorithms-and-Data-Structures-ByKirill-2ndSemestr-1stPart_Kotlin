23784a1ac76e171b21fbb90f56fc56a5e71c0f1a
Kirill <kremizov3@gmail.com>
2025-03-28 10:39:09 +0300

    TrNode (Getters/Setters)
    Added a getters and setters for TrNode class

--- src/main/kotlin/BinTree/TrNode.kt
+++ src/main/kotlin/BinTree/TrNode.kt
@@ -5,4 +5,16 @@ class TrNode<T>() {
     private var _left: TrNode<T>? = null;
     private var _right: TrNode<T>? = null;
 
+    public var value: T?
+        get() = _value;
+        set(value) { _value = value; };
+
+    public var left: TrNode<T>?
+        get() = _left;
+        set(left) {_left = left};
+
+    public var right: TrNode<T>?
+        get() = _right;
+        set(right) { _right = right };
+
 }
