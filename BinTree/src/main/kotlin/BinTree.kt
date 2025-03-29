969717143609726c93f683f29ff7f5ae213f2306
Kirill <kremizov3@gmail.com>
2025-03-29 14:18:35 +0300

    BinTree (Getters/Setters)
    Added Getters and Setters

--- BinTree/src/main/kotlin/BinTree.kt
+++ BinTree/src/main/kotlin/BinTree.kt
@@ -3,4 +3,8 @@ package org.exampl
 class BinTree<U: Comparable<U>>(){
     private var _root: TrNode<U>?= null;
 
+    var root: TrNode<U>?
+        get() = _root;
+        set(node) {_root = node; }
+
 }
